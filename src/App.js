29fa448267ee9f6a75f21790a79c8194577b9727
Cristian Montes <cmontesmujica@gmail.com>
2021-08-08 21:15:00 -0700

    added particle background, working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import Particles from 'react-tsparticles';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm  from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank';
 import './App.css';
 
-function App() {
+const particlesOptions = {
+    // particles: {
+    //   line_linked: {
+    //     shadow: {
+    //       enable: true,
+    //       color: "#3CA9D1",
+    //       blur: 5
+    //     }
+    //   }
+    // },
+    fpsLimit: 100,
+          interactivity: {
+            detectsOn: "canvas",
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          }
+        
+
+}
+
+class App extends Component {
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles className='particles'
+          params={particlesOptions}
+      />
+      <Navigation/>
+      <Logo/>
+      <Rank/>
+      <ImageLinkForm/>
+      {/* <FaceRecognition/> */}
     </div>
   );
+  }
 }
 
 export default App;
