b6e42d41bcc791d9781b929133d94d13d4f86560
Cristian Montes <cmontesmujica@gmail.com>
2021-08-14 16:00:58 -0700

    calculated fucntion to target the face area

--- src/App.js
+++ src/App.js
@@ -73,7 +73,27 @@ const particlesOptions = {
 class App extends Component {
   state ={
     input: '',
-    imageUrl:''
+    imageUrl:'',
+    box:{},
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    // console.log(` witdth=${width}, height=${height}`);
+    return{
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
   }
 
   onInputChange = (event) => {
@@ -83,13 +103,8 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input})
     app.models.predict(Clarifai.FACE_DETECT_MODEL , this.state.input)
-    .then( function(response){
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-    },
-    function(err){
-
-    }
-    );
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
   }
 
   render(){
@@ -102,7 +117,7 @@ class App extends Component {
       <Logo/>
       <Rank/>
       <ImageLinkForm onInputChange = {this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-      <FaceRecognition imageUrl={this.state.imageUrl}/>
+      <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
     </div>
   );
   }
